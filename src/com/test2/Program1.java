4db3698964e559a340355016432be836f42ad310
Rachana2003-hub <rachanakulks@gmail.com>
2025-05-06 09:31:55 +0530

    Update Program1.java

--- src/com/test2/Program1.java
+++ src/com/test2/Program1.java
@@ -1,10 +1,11 @@
 package com.test2;
-
+import java.util.Scanner;
 public class Program1 {
-
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Hello World");
-
+		System.out.println("Enter a number");
+		 int n = sc.nextInt();
+		System.out.println("Square of a number "+n+" is "+n*n);
 	}
-
 }
